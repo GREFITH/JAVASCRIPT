037fd756e5b7c22bba5303d72f7d088e5fdad76f
GREFITH <grefithgohel90@gmail.com>
2024-06-02 10:18:21 +0000

    coversion of datatypes

---
+++ basics/conversionOperations.js
@@ -0,0 +1,40 @@
+// let score = "33"
+// let score = "33abc"
+// let score = true
+// let score = null
+// let score = undefined
+// let score = "ds"
+
+
+
+
+// console.log(typeof score);
+// console.log(typeof(score)); // this way we can write the score in brackets as a methods
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" =>NaN < type is number
+// true => 1; false =>0
+
+// we have to convert the loggedin value to boolean
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "sfd"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=> true
+// 0 => false
+// "" => false
+// "sdf" => true
+
+// string conversion
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
