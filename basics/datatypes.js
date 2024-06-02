4dbdeb596046b055e190afe484b4cbd55fc565dd
GREFITH <grefithgohel90@gmail.com>
2024-06-02 09:46:39 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // trat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+// console.log(3 
+//     + 3); // code redability should be high
+
+// this we can be used to define dataypes. 
+let name = "ds"// string
+let age = 12 // int
+let isLoggedIn = false //boolean
+
+// number => 2 to power 53
+//bigint
+//string
+//boolean => true/false
+// null => standalone value / it is used to represent empty value
+//undefined => value not defined
+//symbol => to find  uniqueness it is most used in react /figma etc
+//object
+
+console.log(typeof "ds");
+console.log(typeof null); // null type is object. 
